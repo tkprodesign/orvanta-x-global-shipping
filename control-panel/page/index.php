437d32b03756678bbc1cb8c1a2b8af381885fa97
Tk Designs <103645938+tkprodesign@users.noreply.github.com>
2026-05-01 03:18:18 +0100

    Update Global Carrier Network copy and visual reference

--- control-panel/page/index.php
+++ control-panel/page/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="/assets/stylesheets/main.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="/assets/stylesheets/control-panel.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="shortcut icon" href="/assets/images/branding/mark-only.png?v=<?php echo time(); ?>" type="image/png">
+    <link rel="shortcut icon" href="/assets/images/branding/favicon.png?v=<?php echo time(); ?>" type="image/png">
     <!-- <script src="https://kit.fontawesome.com/4fee328683.js" crossorigin="anonymous"></script> -->
 </head>
 <body>
--- services/index.php
+++ services/index.php
@@ -15,7 +15,7 @@ include("app.php");
     <link rel="stylesheet" href="/assets/stylesheets/ts/services.css?v=<?php echo time(); ?>" media="screen and (max-width: 1120px)">
     <link rel="stylesheet" href="/assets/stylesheets/ms/main.css?v=<?php echo time(); ?>" media="screen and (max-width: 760px)">
     <link rel="stylesheet" href="/assets/stylesheets/ms/services.css?v=<?php echo time(); ?>" media="screen and (max-width: 760px)">
-    <link rel="shortcut icon" href="<?= htmlspecialchars(asset_url('/assets/images/branding/mark-only.png')); ?>" type="image/png">
+    <link rel="shortcut icon" href="<?= htmlspecialchars(asset_url('/assets/images/branding/favicon.png')); ?>" type="image/png">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
